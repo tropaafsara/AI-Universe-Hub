056c1c88c015caef92b8f50b7b6f771a8ddd3a52
Afsara Tasnim <tratina157@gmail.com>
2023-03-04 14:32:49 +0600

    see more button added

--- js/index.js
+++ js/index.js
@@ -1,23 +1,47 @@
+let allData;
 const fetchAllData = () =>{
     fetch(`https://openapi.programming-hero.com/api/ai/tools`)
     .then(res => res.json())
-    .then(data => showTools(data.data.tools))
+    .then(data =>{
+      allData=data;
+      showTools(data.data.tools.slice(0,6))
+    } )    
 }
-const showTools = data =>{
-    // console.log(data)
+const showTools = (data) =>{
+
+
+  // sort by date
+  /* data.sort(function(a, b) {
+    var c = new Date(a.published_in);
+    var d = new Date(b.published_in);
+    return c-d;
+}); */
+/* document.getElementById('sort-btn').addEventListener('click',data.sort(function(a, b){
+    var c = new Date(a.published_in);
+    var d = new Date(b.published_in);
+    return c-d;
+})) */
+/* document.getElementById('sort-btn').addEventListener('click',function(){
+  
+  data.sort(function(a, b) {
+    var c = new Date(a.published_in);
+    var d = new Date(b.published_in);
+    return c-d;
+});
+}) */
+   
     const toolsContainer =  document.getElementById('tools-container')
+  toolsContainer.innerHTML="";
     data.forEach(singleTool=>{
         // console.log(singleTool);
       //destructuring
       const {id, image,name, published_in, features} = singleTool;
-
+      // singleTool.sort((a, b) => a.published_in - b.published_in);
 
         const toolDiv = document.createElement('div');
         toolDiv.classList.add('col');
-        toolDiv.innerHTML=`
-        
-          
-        <div class="col">
+        toolDiv.innerHTML=`   
+        <div class="col" >
                   <div class="card p-4 rounded-4 ">
                     <img src="${image}" class="card-img-top rounded-4" alt="...">
                     <div class="card-body">
@@ -29,7 +53,7 @@ const showTools = data =>{
                     <hr>
                     <div>
                     <h5 class="card-title">${name}</h5>
-                    <div class="d-flex mx-auto align-items-baseline">
+                    <div class="d-flex mx-auto align-items-baseline justify-content-between">
                     <div class="d-flex gap-2 align-items-baseline ">
                     <i class="fa-solid fa-calendar-days "></i>
                     <p>${published_in}</p>
@@ -44,6 +68,8 @@ const showTools = data =>{
         `
         toolsContainer.appendChild(toolDiv);
     })
+    
+   
 }
 fetchAllData()
 
@@ -57,7 +83,11 @@ const fetchToolDetails = id =>{
 
 const showToolDetails = toolsDetails =>{
   console.log(toolsDetails)
-  const {features,description,integrations,pricing,image_link} = toolsDetails;
+  const {features,description,integrations,pricing,image_link,input_output_examples,accuracy} = toolsDetails;
+  
+  
+  
+
   document.getElementById('modal-body').innerHTML =`
   <div class="row row-cols-1 row-cols-md-2 g-4">
  
@@ -67,34 +97,38 @@ const showToolDetails = toolsDetails =>{
       <div class="card-body">
       <h5 class="card-title mb-3">${description}</h5>
       <div class="d-flex align-items-center justify-content-between mb-3 ">
-                    <div class="p-3 bg-light rounded-4">
-                    <p>${pricing[0].price}</p>
-                    <p>${pricing[0].plan}</p>
+                    <div class="p-3 me-3 bg-light rounded-4 text-success fw-bold ">
+                    <p>${pricing ===null ? "Free Of Cost/" : pricing[0].price }</p>
+                    <p>${pricing === null ? "Basic" : pricing[0].plan}</p>
                     </div>
-                    <div class="p-3 bg-light rounded-4">
-                    <p>${pricing[1].price}</p>
-                    <p>${pricing[1].plan}</p>
+                    <div class="p-3 me-3 bg-light rounded-4 fw-bold" style="color: orange;">
+                    <p>${pricing ===null ? "Free Of Cost/" : pricing[1].price}</p>
+                    <p>${pricing === null ? "Pro" : pricing[1].plan}</p>
                     </div>
-                    <div class="p-4 bg-light rounded-4">
-                    <p>${pricing[2].price}</p>
-                    <p>${pricing[2].plan}</p>
+                    <div class="p-4 bg-light rounded-4 text-danger fw-bold">
+                    <p>${pricing===null ? "Free Of Cost/" : pricing[2].price}</p>
+                    <p>${pricing === null ? "Enterprise" : pricing[2].plan}</p>
                     </div>
                     </div>
                     <div class="d-flex align-items-center">
                     <div>
                     <h5 class="card-title">Features</h5>
+                    
                     <ul>
                     <li>${features[1].feature_name}</li>
                     <li>${features[2].feature_name}</li>
                     <li>${features[3].feature_name}</li>
+                    
                   </ul>
                     </div>
                     <div>
                     <h5 class="card-title">Integrations</h5>
                       <ul>
-                        <li>${integrations[0] ? integrations[0] : "No Data Found"}</li>
-                        <li>${integrations[1] ? integrations[1] : "No Data Found"}</li>
-                        <li>${integrations[2] ? integrations[2] : "No Data Found"}</li>
+                        <li>${integrations===null ?   "No Data Found" : integrations[0]}</li>
+                        <li>${integrations===null ?   "No Data Found" : integrations[1]}</li>
+                        <li>${integrations===null ?   "No Data Found" : integrations[2]}</li>
+                        
+
                       </ul>                     
                     </div>
                     </div>
@@ -102,18 +136,15 @@ const showToolDetails = toolsDetails =>{
       </div>
     </div>
   </div>
-
-
-
-
-
-
   <div class="col">
-    <div class="card">
-      <img src="${image_link[0]}" class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    <div class="card rounded-3">
+      <div class="container position-relative">
+      <img src="${image_link[0]}" class="card-img-top p-3 rounded-3" alt="...">
+      <button id="no-found-message" class="btn btn-danger position-absolute top-0 end-0 translate-middle-x" style="width:150px;">${  accuracy.score*100}% accuracy</button>
+      </div>
+      <div class="card-body text-center ">
+        <h5 class="card-title ">${input_output_examples === null ? "Can you give any example?" : input_output_examples[0].input}</h5>
+        <p class="card-text">${input_output_examples===null ? "No! Not Yet! Take a Break" : input_output_examples[0].output }</p>
       </div>
     </div>
   </div>
@@ -121,3 +152,31 @@ const showToolDetails = toolsDetails =>{
                     
   `;
 }
+
+/* const toggleSpinner = isLoading =>{
+  const loaderSection = document.getElementById('loader');
+  if(isLoading){
+      loaderSection.classList.remove('d-none')
+  }else{
+      loaderSection.classList.add('d-none')
+  }
+} */
+
+const showAllData =() =>{
+  
+
+  fetch(`https://openapi.programming-hero.com/api/ai/tools`)
+  .then(res => res.json())
+  .then(data =>{
+    allData=data;
+    showTools(data.data.tools)
+  } )  
+  const seeMore = document.getElementById('see-more');
+  seeMore.classList.add('d-none');
+}
+
+
+
+
+
+
